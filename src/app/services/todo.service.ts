c0d20561b5ff148c8e62eb78cb14de81b72dd675
louis <quocbinhgithub@gmail.com>
2023-09-01 16:58:41 +0700

    feat: fetch from local storage

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Filter } from '../models/filtering.model';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private static readonly TodoStorageKey = 'todos';
+  private todos: Todo[];
+  private filterTodos: Todo[];
 
-  constructor() { }
+  private lengthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(
+    0
+  );
+  private displayTodoSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<
+    Todo[]
+  >([]);
+  private currentFilter: Filter = Filter.All;
+
+  todos$: Observable<Todo[]> = this.displayTodoSubject.asObservable();
+  length$: Observable<number> = this.lengthSubject.asObservable();
+  constructor(private storeService: LocalStorageService) {}
+  fetchFromLocalStorage() {
+    this.todos =
+      this.storeService.getValue<Todo[]>(TodoService.TodoStorageKey) || ơ;
+    this.filterTodos = [...this.todos];
+
+    this.displayTodoSubject.next(this.filterTodos);
+    this.lengthSubject.next(this.todos.length);
+  }
 }
