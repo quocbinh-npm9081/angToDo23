6f7831a6ba753e8b51a697f3b45ddc9c54c74611
louis <quocbinhgithub@gmail.com>
2023-09-01 07:38:05 +0700

    feat: todo item component

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent {
-
+  isHovering = false;
+  isEditing = false;
 }
